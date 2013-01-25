992d0b657152de2620351490d8bfa2d463d31e56
Marvin Thomas Rabe <mrabe@marvinrabe.de>
2013-01-25 10:52:22 +0100

    Updated design.

--- add_google.js
+++ add_google.js
@@ -1,9 +1,20 @@
 
+var button_id = 0;
+
+function addButton(link, label) {
+    $('#side').prepend('<div class="button ddf' + button_id + '"><span class="trig_add_browser" id="trig_add_to_browser"></span><a href="' + link + '" class="button-link">' + label + '</a></div>');
+    $('.ddf' + button_id).bind('click', function() {
+        location.href = link;
+    });
+    button_id += 1;
+}
+
 $(document).ready(function () {
 	var search = $("#search_form_input").val();
 
 	if(search != undefined) {
-		$('#side div:first').prepend('<a href="https://encrypted.google.com/search?q=' + encodeURI(search) + '" target="_blank">Google it!</a><br>');
+        var encodedSearch = encodeURI(search);
+        addButton('https://encrypted.google.com/search?q=' + encodedSearch, 'Google it!');
 	}
 });
 
