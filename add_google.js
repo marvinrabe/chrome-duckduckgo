7ad9cab8bea2cf195874e13cdcdbd876a9193391
Marvin Thomas Rabe <mrabe@marvinrabe.de>
2013-02-01 20:49:39 +0100

    Icon added.

--- add_google.js
+++ add_google.js
@@ -2,19 +2,17 @@
 var button_id = 0;
 
 function addButton(link, label) {
-    $('#side').prepend('<div class="button ddf' + button_id + '"><span class="trig_add_browser" id="trig_add_to_browser"></span><a href="' + link + '" class="button-link">' + label + '</a></div>');
+    $('#side').prepend('<div class="button ddf' + button_id + '"><span class="trig_search"></span><a href="' + link + '" class="button-link">' + label + '</a></div>');
     $('.ddf' + button_id).bind('click', function() {
         location.href = link;
     });
     button_id += 1;
 }
 
-$(document).ready(function () {
-	var search = $("#search_form_input").val();
+var search = $("#search_form_input").val();
 
-	if(search != undefined) {
-        var encodedSearch = encodeURI(search);
-        addButton('https://encrypted.google.com/search?q=' + encodedSearch, 'Google it!');
-	}
-});
+if(search != undefined) {
+    var encodedSearch = encodeURI(search);
+    addButton('https://encrypted.google.com/search?q=' + encodedSearch, 'Search on Google');
+}
 
