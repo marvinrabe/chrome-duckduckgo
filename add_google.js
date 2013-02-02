8890d18ff1005096d33d026df6aa1b052fbbd8a0
Marvin Thomas Rabe <mrabe@marvinrabe.de>
2013-02-02 01:33:26 +0100

    Removed jQuery.

--- add_google.js
+++ add_google.js
@@ -1,19 +1,45 @@
 
-var button_id = 0;
+var searchField = document.getElementById("search_form_input");
 
-function addButton(link, message) {
-    var label = chrome.i18n.getMessage(message);
-    $('#side').prepend('<div class="button ddf' + button_id + '"><span class="trig_search"></span><a href="' + link + '" class="button-link">' + label + '</a></div>');
-    $('.ddf' + button_id).bind('click', function() {
-        location.href = link;
-    });
-    button_id += 1;
+function getAddButtonFunction(searchQuery) {
+
+    var sideBar = document.getElementById("side");
+
+    // Span class is needed for layout reasons
+    var icon = document.createElement("span");
+    icon.className = "trig_search";
+
+    function addButton(link, message) {
+        var label = chrome.i18n.getMessage(message);
+        var searchURL = link + searchQuery;
+
+        var text = document.createTextNode(label);
+
+        // Create link
+        var link = document.createElement("a");
+        link.className = "button-link";
+        link.setAttribute("href", searchURL); 
+        link.appendChild(text);
+
+        // Create div
+        var element = document.createElement("div");
+        element.className = "button";
+        // Trigger click if div was clicked
+        element.setAttribute("onclick", "this.childNodes[1].click()"); 
+        element.appendChild(icon);
+        element.appendChild(link);
+
+        // Prepend div before default DuckDuckGo buttons
+        sideBar.insertBefore(element,sideBar.childNodes[0]);
+
+    }
+
+    return addButton;
 }
 
-var searchQuery = $("#search_form_input").val();
-
-if(searchQuery != undefined) {
-    var encodedSearchQuery = encodeURI(searchQuery);
-    addButton('https://encrypted.google.com/search?q=' + encodedSearchQuery, 'google');
+if(undefined != searchField.value) {
+    var encodedSearchQuery = encodeURI(searchField.value);
+    var addButton = getAddButtonFunction(encodedSearchQuery)
+    addButton('https://encrypted.google.com/search?q=', 'google');
 }
 
